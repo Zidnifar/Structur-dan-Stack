3a80a43a71f55d1c81116606b42821dab99e76f8
Zidnifar <zidnifarahmazida@gmail.com>
2025-05-28 14:37:42 +0800

    Membuat isi kode program

--- c030324114stack.cpp
+++ c030324114stack.cpp
@@ -10,28 +10,34 @@ typedef struct{
 } Stack;
 
 void InitializeStack(Stack *S){
-    
+    S->Count = 0;
 }
 
 int Full(Stack *S){
-   
+    return (S->Count == MAXSTACK);
 }
 
 int Empty(Stack *S){
-   
+    return (S->Count == 0);
 }
 
 void Push(Stack *S, ItemType X){
-     
+    if(S->Count < MAXSTACK){
+        S->Item[S->Count++] = X; // Increment Count after assignment
+    }else {
+        cout << "Stack penuh! Data tidak dapat masuk!" << endl;
+    }
 }
 
 void Pop(Stack *S, ItemType *X){
-
+    if(S->Count > 0){
+        *X = S->Item[--S->Count]; // Decrement Count before assignment
+    }else{
+        cout << "Stack masih kosong!" << endl;
+    }
 }
-    
-
 
 int main(){
+    
     return 0;
 }
-    
