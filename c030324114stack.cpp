1da106b4e025956e554319f205046f4294070afc
Zidnifar <zidnifarahmazida@gmail.com>
2025-05-28 12:45:27 +0800

    Menambahkan file stack

---
+++ c030324114stack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+typedef int ItemType;
+const int MAXSTACK = 15;
+
+typedef struct{
+    ItemType Item[MAXSTACK];
+    int Count;
+}Stack;
+
+int main(){
+    return 0;
+}
